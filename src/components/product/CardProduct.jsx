2916db6afc12d050f97ce118ce9be15c63c05967
Agustin Leguizamon <agustin.leg7@gmail.com>
2022-05-07 09:21:19 -0300

    💅 v2

--- src/components/product/CardProduct.jsx
+++ src/components/product/CardProduct.jsx
@@ -10,71 +10,68 @@ import {
 
 function CardProduct({ item, addToCart }) {
   return (
-    <Flex
-      alignItems="center"
-      justifyContent="center"
+    <Box
+      bg={useColorModeValue("white", "gray.800")}
+      borderWidth="1px"
+      rounded="lg"
+      shadow="lg"
+      position="relative"
       w="100%"
       maxW="300px"
-      h="lg"
+      h="sm"
+      p={2}
+      d="flex"
+      flexDirection="column"
     >
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
-        w="full"
-        h="full"
-      >
+      <Box h="50%">
         <Image
           src={item.image}
           alt={`Picture of ${item.name}`}
           roundedTop="lg"
           objectFit="cover"
-          width="75%"
-          height="xs"
+          height="full"
           margin="auto"
         />
-
-        <Box p="6">
-          <Box d="flex" alignItems="baseline">
-            {item.isNew && (
-              <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
-                New
-              </Badge>
-            )}
-          </Box>
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontSize="lg"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {item.title}
-            </Box>
-          </Flex>
-
-          <Flex justifyContent="space-between" alignContent="center">
-            <Box fontSize="xl" color={useColorModeValue("gray.800", "white")}>
-              <Text as="span" color={"gray.600"} fontSize="lg">
-                $
-              </Text>
-              {item.price}
-            </Box>
-          </Flex>
-          <Button
-            w="full"
-            colorScheme="blue"
-            mt={2}
-            onClick={() => addToCart(item)}
-          >
-            Agregar al carrito
-          </Button>
-        </Box>
       </Box>
-    </Flex>
+
+      <Box p="6" flex="1 auto">
+        <Box d="flex" alignItems="baseline">
+          {item.isNew && (
+            <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
+              New
+            </Badge>
+          )}
+        </Box>
+        <Flex mt="1" justifyContent="space-between" alignContent="center">
+          <Box
+            fontSize="lg"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            {item.title}
+          </Box>
+        </Flex>
+
+        <Flex justifyContent="space-between" alignContent="center">
+          <Box fontSize="xl" color={useColorModeValue("gray.800", "white")}>
+            <Text as="span" color={"gray.600"} fontSize="lg">
+              $
+            </Text>
+            {item.price}
+          </Box>
+        </Flex>
+      </Box>
+      <Button
+        w="full"
+        colorScheme="blue"
+        mt={2}
+        onClick={() => addToCart(item)}
+      >
+        Agregar al carrito
+      </Button>
+    </Box>
   );
 }
 
